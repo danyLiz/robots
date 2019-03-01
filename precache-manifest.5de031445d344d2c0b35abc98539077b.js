385ea6df8878e5c941350b12b868b142a5765db8
danyLiz <danielastelia@yahoo.com>
2019-03-01 17:46:47 +0200

    Updates

---
+++ precache-manifest.5de031445d344d2c0b35abc98539077b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "df3b9b2a91cac00616ea",
+    "url": "/static/js/main.df3b9b2a.chunk.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "df3b9b2a91cac00616ea",
+    "url": "/static/css/main.b44232ab.chunk.css"
+  },
+  {
+    "revision": "0e5616657e628f148e740cd71eff03d4",
+    "url": "/index.html"
+  }
+];
